dc8b507e0a85d6e49dafc231fe4190a23fbafda9
ewartboonstra <e.boonstra@hotmail.nl>
2015-04-13 16:13:25 +0200

    mainScreen. doesn't work currently

--- BPP Simulator/src/MainScreen.java
+++ BPP Simulator/src/MainScreen.java
@@ -2,21 +2,31 @@ import javax.swing.*;
 
 /**
  * Created by Eldin on 4/9/2015 for Windesheim Magazijn Robot KBS
- * In this class the mainscreen will be created
+ * In this class the mainScreen will be created
  */
 public class MainScreen extends JFrame {
 
-    private JFrame mainscreen;
+    private JFrame mainScreen;
+    private JPanel topButtons, bins;
 
     public MainScreen(){
-        mainscreen = new JFrame("BPP Simulator");
-        mainscreen.setSize(1024, 768);
-        mainscreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainscreen.setVisible(true);
+        mainScreen = new JFrame("BPP Simulator");
+        mainScreen.setSize(1024, 768);
+        mainScreen.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainScreen.setVisible(true);
+        JPanel topButtons = new JPanel();
+        JPanel bins = new JPanel();
+        this.bins = bins;
+        JPanel parent = new JPanel();
+        topButtons.add(new JButton("test"));
+        parent.add(topButtons);
+        parent.add(bins);
+        mainScreen.add(parent); //remove parent to change it to old setup
     }
 
     public void addToScreen(Drawer draw){
-        mainscreen.add(draw);
+        bins.add(draw); //you can't put it directly in the JFrame because we also need buttons etc. in here.
+        //mainScreen.add(draw);
     }
 
 }
